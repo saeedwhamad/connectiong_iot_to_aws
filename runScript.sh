342c00e5f2466ac29b3af47c33d15ff86b132288
saeedwhamad <93330415+saeedwhamad@users.noreply.github.com>
2024-05-23 11:11:13 +0300

    Update runScript.sh

--- runScript.sh
+++ runScript.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 pip install AWSIoTPythonSDK
-openssl genpkey -algorithm RSA -out device_private3.key
-openssl req -new -key device_private3.key -out device.csr
+openssl genpkey -algorithm RSA -out device_private.key
+openssl req -new -key device_private.key -out device.csr
 python3 code.py "/device.csr"
